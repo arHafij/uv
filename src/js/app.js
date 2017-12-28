724c4c88df9fd5203aa62a3830be61a46848aa1c
arhafij <arhafij100@gmail.com>
2017-12-28 11:28:25 +0600

    Before transfer gitlab

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+//@prepros-append "../../node_modules/jquery/dist/jquery.js"
+//@prepros-append "../../node_modules/popper.js/dist/umd/popper.js"
+//@prepros-append "../../node_modules/bootstrap/dist/js/bootstrap.js"
